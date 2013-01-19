c07f53ac067b8a23f1b5f1603a10c1b8aa0c9c41
ajlopez <webmaster@ajlopez.com>
2013-01-19 16:59:02 -0300

    Web Crawler sample

---
+++ samples/webcrawler/run.js
@@ -0,0 +1,86 @@
+
+var mproc = require('../..'),
+    http = require('http'),
+    url = require('url');
+
+function resolver(link) {
+    var hostname = url.parse(link).hostname;
+    var visited = {};
+
+    return function (message, context, next) {
+        var urldata = url.parse(message);
+
+        if (urldata.hostname !== hostname)
+            return;
+
+        if (visited[message])
+            return;
+
+        visited[message] = true;
+
+        next(null, message);
+    };
+}
+
+function download(message, context, next) {
+    var urldata = url.parse(message);
+
+    options = {
+        host: urldata.hostname,
+        port: urldata.port,
+        path: urldata.path,
+        method: 'GET'
+    };
+
+    http.get(options, function(res) { 
+            console.log('Downloading:', message);
+            res.setEncoding('utf8');
+            var body = '';
+
+            res.on('data', function(data) {
+                body += data;
+            });
+
+            res.on('end', function() {
+                console.log('Download complete:', message);
+                next(null, body);
+            });
+       }).on('error', function(e) {
+            console.log('Url:', link);
+            console.log('Error:', e.message);
+            next(e);
+        });
+}
+
+function harvest(message, context, next) {
+    var match1 = /href=\s*"([^&"]*)"/ig;
+    var match2= /href=\s*'([^&']*)'/ig;
+
+    while ((matchdata = match1.exec(message)) !== null) {
+        var link = matchdata[1];
+        if (link.indexOf('http:') == 0)
+            context.post(link);
+    }
+
+    while ((matchdata = match2.exec(message)) !== null) {
+        var link = matchdata[1];
+        if (link.indexOf('http:') == 0)
+            context.post(link);
+    }
+}
+
+// Process arguments
+
+process.argv.forEach(function(arg) {
+    if (arg.indexOf("http:")==0)
+    {
+        var processor = mproc.createProcessor();
+
+        processor.use(resolver(arg))
+            .use(download)
+            .use(harvest);
+
+        processor.run(arg);
+    }
+});
+
