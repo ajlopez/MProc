16704933b2fe7f482058dedecfec7a392c19ded7
ajlopez <webmaster@ajlopez.com>
2013-01-19 15:31:58 -0300

    chained use

--- test/processor.js
+++ test/processor.js
@@ -10,8 +10,8 @@ exports['createProcessor defined'] = function (test) {
 exports['define and sync simple run'] = function (test) {
     var processor = mproc.createProcessor();
 
-    processor.use(function (message, context, next) { message++; next(null, message); });
-    processor.use(function (message) { test.equal(2, message); test.done() });
+    processor.use(function (message, context, next) { message++; next(null, message); })
+        .use(function (message) { test.equal(2, message); test.done() });
 
     processor.runSync(1);
 };
@@ -21,9 +21,27 @@ exports['define and sync simple run with two steps'] = function (test) {
 
     function incmsg(message, context, next) { message++; next(null, message); }
 
-    processor.use(incmsg);
-    processor.use(incmsg);
-    processor.use(function (message) { test.equal(3, message); test.done() });
+    processor.use(incmsg)
+        .use(incmsg)
+        .use(function (message) { test.equal(3, message); test.done() });
+
+    processor.runSync(1);
+};
+
+exports['context send for loop'] = function (test) {
+    var processor = mproc.createProcessor();
+
+    function incmsg(message, context, next) { 
+        test.ok(message > 0); 
+        message++; 
+        if (message == 3)
+            next(null, message);
+        else
+            context.send(message);
+    }
+
+    processor.use(incmsg)
+        .use(function (message) { test.equal(3, message); test.done() });
 
     processor.runSync(1);
 };
--- test/run.js
+++ test/run.js
@@ -5,8 +5,8 @@ exports['define and simple run'] = function (test) {
     test.expect(1);
     var processor = mproc.createProcessor();
 
-    processor.use(function (message, context, next) { message++; next(null, message); });
-    processor.use(function (message) { test.equal(2, message); test.done() });
+    processor.use(function (message, context, next) { message++; next(null, message); })    
+        .use(function (message) { test.equal(2, message); test.done() });
 
     processor.run(1);
 };
@@ -18,9 +18,29 @@ exports['define and simple run with two steps'] = function (test) {
 
     function incmsg(message, context, next) { test.ok(message > 0); message++; next(null, message); }
 
-    processor.use(incmsg);
-    processor.use(incmsg);
-    processor.use(function (message) { test.equal(3, message); test.done() });
+    processor.use(incmsg)
+        .use(incmsg)
+        .use(function (message) { test.equal(3, message); test.done() });
 
     processor.run(1);
+};
+
+exports['context post for loop'] = function (test) {
+    test.expect(3);
+
+    var processor = mproc.createProcessor();
+
+    function incmsg(message, context, next) { 
+        test.ok(message > 0); 
+        message++; 
+        if (message == 3)
+            next(null, message);
+        else
+            context.post(message);
+    }
+
+    processor.use(incmsg)    
+        .use(function (message) { test.equal(3, message); test.done() });
+
+    processor.runSync(1);
 };
