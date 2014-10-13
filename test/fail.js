95bd2140b898ced5c544fa383055df4e054bbdef
ajlopez <webmaster@ajlopez.com>
2014-10-13 06:41:21 -0300

    fail with tests

---
+++ test/fail.js
@@ -0,0 +1,29 @@
+
+var mproc = require('..');
+
+exports['next with err throw exception'] = function (test) {
+    var processor = mproc.createProcessor();
+
+    processor.use(function (message, next) { message++; next('error'); })    
+
+    try {
+        processor.run(1);
+        test.ok(null);
+    }
+    catch (ex) {
+        test.ok(ex);
+        test.equal(ex, 'error');
+    }
+};
+
+exports['next with err using fail'] = function (test) {
+    var processor = mproc.createProcessor();
+
+    processor.use(function (message, next) { message++; next('error'); })
+        .fail(function (err) {
+            test.ok(err);
+            test.equal(err, 'error');
+        });
+        
+    processor.run(1);
+};
